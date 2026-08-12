b6709c0df5c1e8a81616477e5ca17110949b5770
Anant Singh Shishodia <anantthakur2005@gmail.com>
2026-08-12 21:15:24 +0530

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.cpp
@@ -0,0 +1,47 @@
+//Time complexity O(n)and space O(1)
+class Solution {
+public:
+    bool isPalindrome(string s) {
+
+        int i = 0;
+        int j = s.length() - 1;
+
+        while(i < j)
+        {
+            while(i < j && !isalnum(s[i]))
+                i++;
+
+            while(i < j && !isalnum(s[j]))
+                j--;
+
+            if(tolower(s[i]) != tolower(s[j]))
+                return false;
+
+            i++;
+            j--;
+        }
+
+        return true;
+    }
+};
+
+
+//T.C O(n) S.C O(1)
+// class Solution {
+//    public:
+//     bool isPalindrome(string s) {
+//         for (char& ch : s) {
+//             ch = tolower(ch);
+//         }
+//         string s1 = "";
+//         for (auto ch : s) {
+//             if (isalpha(ch)) {
+//                 s1.push_back(ch);
+//             }
+//         }
+//         string s2 = s1;
+//         reverse(s2.begin(), s2.end());
+//         return s2 == s1;
+//     }
+// };
+
